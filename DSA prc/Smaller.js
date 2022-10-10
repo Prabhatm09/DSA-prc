841b85d0369fd9f05e1eb75375c86b24cb160c84
Prabhat mishra <105411899+Prabhatm09@users.noreply.github.com>
2022-10-11 01:23:50 +0530

    smaller

--- DSA prc/Smaller.js
+++ DSA prc/Smaller.js
@@ -1,13 +1,17 @@
 var smallerNumbersThanCurrent = function(nums) {
-    let count = 0
+    let newarr = [];
     for(let current=0; current<nums.length; current++){
-        for(let small=current+1; small<nums.length; small++){
+        let count = 0
+        for(let small=0; small<nums.length; small++){
             if(nums[current] > nums[small]){
-                console.log(count++)
-
+                // console.log(count++)
+                count++
+            }   
         }
+        newarr.push(count)
     }
-    // return count++
-    }
+    return newarr
 };
-console.log(smallerNumbersThanCurrent([8,1,2,2,3]));
+console.log(smallerNumbersThanCurrent([6,1,7,2,3]));
+
+// [4, 0 ,1 ,1, 2]
